6370a2ff07f4c3f90847c6cbfdb4729006d18f63
skconan <supakit.kr@gmail.com>
2017-09-10 22:32:17 +0700

    test sending email (pass)

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.conf import settings
+from django.core.mail import send_mail
+
+
 # Create your views here.
 def contact(req):
+    title = 'Contact'
     form = ContactForm(req.POST or None)
+    context = {'title':title, 'form':form, }
     if form.is_valid():
-        print(req.POST)
+        name = form.cleaned_data['name']
+        comment = form.cleaned_data['comment']
+        subject = 'Message from E-Commerce App'
+        message = '%s %s'%(comment, name)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(subject,message,emailFrom,emailTo,fail_silently=True)
     context = locals()
     template = 'contact.html'
-    return render(req,template,context)
+    return render(req, template, context)
