6b02e69a006a5f17b593fa4b4699681545fca23f
skconan <supakit.kr@gmail.com>
2017-09-10 17:41:39 +0700

    created job feild

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,6 +5,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(default='description default text')
     location = models.CharField(max_length=128,default='my location default text')
+    job = models.CharField(max_length=128,null=True)
     
     def __str__(self):
         return self.name
